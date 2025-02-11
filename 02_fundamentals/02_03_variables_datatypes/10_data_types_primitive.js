2b1c880a69cee980a81a2fb6599d7c80626b4804
jo.nesbo.oslo@gmail.com <jo.nesbo.oslo@gmail.com>
2025-02-11 11:16:58 -0600

    fundamentals

---
+++ 02_fundamentals/02_03_variables_datatypes/10_data_types_primitive.js
@@ -0,0 +1,25 @@
+// NUmber
+let n = 42;
+let pi = 3.14;
+
+// String
+let stringDoubleQuote = "Hello, World!";
+let stringSingleQuote = 'Hello, World!';
+
+// Boolean
+let bool= true;
+
+// Undefined
+let notAssigned;
+console.log(notAssigned);
+
+// null
+let empty = null;
+
+// Symbol
+let sym = Symbol('unique');
+
+// BigInt
+let bigNumber = 123456789012345678901234567890n;
+
+console.log(Number.MAX_SAFE_INTEGER);
--- 02_fundamentals/02_05_type_conversion_coercion/24_type_conversion.js
+++ 02_fundamentals/02_05_type_conversion_coercion/24_type_conversion.js
@@ -2,15 +2,23 @@
 let s = "123";
 let n = Number(s);  // Converts string to number
 console.log(n);  
+n = parseInt(s);
+console.log(n);  
+n = parseFloat(s);
+console.log(n);  
 
 // 2. Number to String
 let n = 123;
 let s = String(n);  //Converts number to string
 console.log(s);  
+s = n + '';
+console.log(s);
 
 // 3. Boolean to Number
 let bool = true;
 let n = Number(bool);  //Converts boolean to number
+console.log(n);
+n = Number(!bool);
 console.log(n);
 
 // 4. Boolean to String
