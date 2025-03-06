694fe4686898ebc0c0d432cc5c963cddda07b44a
jo.nesbo.oslo@gmail.com <jo.nesbo.oslo@gmail.com>
2025-03-05 18:57:15 -0600

    json

---
+++ 08_json/03_read_json_file/02_require.js
@@ -0,0 +1,3 @@
+// Node.js example
+const data = require('./json/02_require.json');
+console.log(data);
---
+++ 08_json/03_read_json_file/03_import.mjs
@@ -0,0 +1,5 @@
+import jsonData from './json/03_import.json' assert { type: 'json' };
+console.log(jsonData);
+
+// to run
+// node --experimental-json-modules .\08_json\03_read_json_file\03_import.mjs 
